8f704614594e5c80635033b5d99eb7b1ec0b2550
Adelina Sharafutdinova <adelisha.dinov@gmail.com>
2025-03-24 17:30:15 +0300

    add: 012 c# легкий квест отпрос

---
+++ csharp/012-questionnaire/Program.cs
@@ -0,0 +1,21 @@
+﻿ushort points = 0;
+
+Console.WriteLine("Что означает слово 'cat'? [кошка, собака]");
+var answer = Console.ReadLine();
+if(answer == "кошка")
+{
+    Console.WriteLine("Всё верно! 😎");
+    points += 10;
+} 
+else Console.WriteLine("Нужно подумать ещё 🫣");
+
+Console.WriteLine("Что означает слово 'dog'? [кошка, собака]");
+answer = Console.ReadLine();
+if(answer == "собака") 
+{
+    Console.WriteLine("Всё верно! 😎");
+    points += 10;
+}
+else Console.WriteLine("Нужно подумать ещё 🫣");
+
+Console.WriteLine("Счет: " + points);
