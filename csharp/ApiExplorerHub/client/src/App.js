ad2adcb06899f1a6df2f3006c9f0a4a76e6cd710
Adelina Sharafutdinova <adelisha.dinov@gmail.com>
2025-03-26 17:20:49 +0300

    Начло фронтенда

--- csharp/ApiExplorerHub/client/src/App.js
+++ csharp/ApiExplorerHub/client/src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div>
+        <div>
+          <h1>Список контактов</h1>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Имя</th>
+                <th>E-mail</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              <tr>
+                <th>1</th>
+                <th>Имя 3</th>
+                <th>example1@gmail.com</th>
+              </tr>
+
+              <tr>
+                <th>2</th>
+                <th>Имя 2</th>
+                <th>example2@gmail.com</th>
+              </tr>
+
+              <tr>
+                <th>3</th>
+                <th>Имя 1</th>
+                <th>example3@gmail.com</th>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+
     </div>
   );
 }
